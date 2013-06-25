3b1132e91038f46419111a168461c3933ced702b
pjcdevops <pjcdevops@gmail.com>
2013-06-25 01:36:49 +0100

    encapsculated logic to curl/config functions

--- lib/projectyoda.py
+++ lib/projectyoda.py
@@ -1,28 +1,35 @@
 import sys, subprocess, resources, configs
-# should wrap curl and config calls in to functions as curl will
-# will be duplicated with and without -c switch
 
+# curl function
+def curl_resource(argv_offset):
+    for i in range (argv_offset, len(sys.argv)):
+        resource = resources.return_resource(sys.argv[i])
+        if resource != False:
+            print 'Grabbing ' + sys.argv[i]
+            curl_req = subprocess.Popen(['curl', '-LO', resource])
+            curl_req.wait() # wait for subprocess to finish before continuing with parent process
+        else:
+            print 'Invalid resource specified: ' + sys.argv[i]
+
+# config function
+def run_config(argv_offset):
+    config = []
+    for i in range(argv_offset, len(sys.argv)):
+        config.append(sys.argv[i])
+    config_script = configs.return_config(config)
+    if config_script != False:
+        print 'valid config'
+    else:
+        print 'invalid config:'
+        print config
+
+# execution
 if len(sys.argv) > 1:
     if sys.argv[1] == '-c': # check for config switch
-        for i in range (2, len(sys.argv)):
-            resource = resources.return_resource(sys.argv[i])
-            if resource != False:
-                print 'Grabbing ' + sys.argv[i]
-                curl_req = subprocess.Popen(['curl', '-LO', resource])
-                curl_req.wait() # wait for subprocess to finish before continuing with parent process
-            else:
-                print 'Invalid resource specified: ' + sys.argv[i]
-        config = []
-        for i in range(2, len(sys.argv)):
-            config.append(sys.argv[i])
-        config_script = configs.return_config(config)
-        if config_script != False:
-            print 'valid config'
-        else:
-            print 'invalid config:'
-            print config
+        curl_resource(2)
+        run_config(2)
     else:
-        print 'we are running dl only'
+        curl_resource(1)
 else: 
     print 'no args - should show help function'
 
--- lib/test.py
+++
@@ -1,5 +0,0 @@
-if set([1, 2]) == set([2, 1]):
-    print 'equal'
-else:
-    print 'not equal'
-
