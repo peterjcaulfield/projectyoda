0cf2e2e844aeb356fe2a94d1658082333b0c9217
pjcdevops <pjcdevops@gmail.com>
2013-06-26 23:44:15 +0100

    implemented contract between init and yoda methods (parse input to format/deal with formatted input) and moved to exceptions instead of sys.exit())

--- lib/projectyoda.py
+++ lib/projectyoda.py
@@ -1,66 +1,106 @@
 ''' 
 to do:
-    - check for valid config if a -c switch is specified in init?
+    
+    - move checks relative to individual methods to those methods
+      i.e grab should check if a resource is valid not the init
+
 '''
 
 import sys, os, subprocess, re, resources, configs
 
 valid_commands = ['grab', 'resources', 'configs', 'help']
 
+class yodaError(Exception):
+    def __init__(self, value):
+        self.value = value
+
 # parse args and set state for config
 def init():
-    env = { 'args' : [], 'resources' : [], 'cmd' : False, 'switch' : False}
+    # format args
+    env = { 'args' : [], 'cmd' : False, 'switch' : False}
     sys.argv.pop(0) # we dont need path
-    for arg in sys.argv:
-        env['args'].append(arg) # meh. feels cleaner to me.
-    
-    if env['args'][0] in valid_commands: # check cmd is valid
+    if len(sys.argv) < 1:
+        error_object = []
+        error_object.append('No arguments provided. See yoda help for cmd format')
+        raise yodaError(error_object)
+    # check if cmd is valid
+    if sys.argv[0] in valid_commands:
+        env['cmd'] = sys.argv[0]
         
-        if len(env['args']) > 1: # make sure we have minimum amount of args to check for switch/resource
-            env['cmd'] = env['args'][0]
-            
-            if re.match('^-', env['args'][1]): # check for switch
-                
-                if len(env['args']) > 2: # check for resource
-                    env['switch'] = env['args'][1]
-                    sys.argv.pop(0) # pop cmd
-                    sys.argv.pop(0) # pop switch
-                    env['resources'] = sys.argv # left with specified resources
-                else: 
-                    print 'No resource specified. Please see yoda help for cmd format' # no resource specified after switch
-                    return False
-            
-            else:
-                sys.argv.pop(0) # pop cmd
-                env['resources'] = sys.argv # left with specified resources
-            
-            invalid_resources = [] # check if resources exist
-            valid_resources = 0
-            for resource in env['resources']:
-                valid_resource = resources.return_resource(resource)
-                if valid_resource:
-                    valid_resources += 1
+        # check if we have a switch
+        if len(sys.argv) > 1:
+            if re.match('^-', sys.argv[1]):
+                env['switch'] = sys.argv[1]
+                # store remaning args if present and return env
+                if len(sys.argv) > 2:
+                    for i in range(2, len(sys.argv)):
+                        env['args'].append(sys.argv[i])
+                    print env
+                    return env
                 else:
-                    invalid_resources.append(resource)
-            if (len(env['resources']) != valid_resources):
-                print 'Invalid resources specified:'
-                print invalid_resources
-                print 'Please see yoda resources for available resources'
-                return False
-            else: # we have valid resources
+                    return env
+            else: # no switch present
+                for i in range(1, len(sys.argv)):
+                    env['args'].append(sys.argv[i])
                 print env
                 return env
-        
-        else:
-            print 'No resource specified. Please see yoda help for cmd format'
-            return False
+        else: # no switch or args to cmd
+            print env
+            return env
     
     else:
-        print 'Invalid command: ' + env['args'][0]
-        print 'Valid commands are:'
-        print valid_commands
-        return False
+        error_object = []
+        error_object.append('Invalid command specified: ' + sys.argv[0])
+        error_object.append('Valid commands are: ')
+        error_object.append(valid_commands)
+        raise yodaError(error_object)
 
+     
+
+def grab(args):
+    valid_switches = ['-c']
+    # check we have args
+    if len(args['args']) < 1:
+        error_object = []
+        error_object.append('No resource specified')
+        raise yodaError(error_object)
+    else: # check resources
+        check_resources(args['args'])
+        print 'all resources valid'
+        print args
+    # check if a switch was passed
+    if args['switch']:
+        # check if switch is valid
+        if args['switch'] in valid_switches:
+            print 'running code with switch: ' + args['switch']
+            curl_resource(args['args'])
+            # switch is valid so check if resources are valid
+        else: # invalid switch given
+            error_object = []
+            error_object.append('Invalid switch specifed: ' + args['switch'])
+            raise yodaError(error_object)
+    # no switch passed
+    else:
+        print 'running without switch'
+        curl_resource(args['args'])
+            
+def check_resources(args):
+        invalid_resources = [] 
+        valid_resources = 0
+        for resource in args:
+            valid_resource = resources.return_resource(resource)
+            if valid_resource:
+                valid_resources += 1
+            else:
+                invalid_resources.append(resource)
+        if valid_resources != len(args):
+            error_object = []
+            error_object.append('Invalid resources specified:')
+            error_object.append(invalid_resources)
+            error_object.append('Please see yoda resources for available resources')
+            raise yodaError(error_object)
+        else: # we have valid resources
+            return True
 
 # curl function
 def curl_resource(args):
@@ -71,6 +111,7 @@ def curl_resource(args):
         #curl_req.wait() # wait for subprocess to finish before continuing with parent process
         #curl_result['curl_completes'].append(arg)
         return True
+
 
 # unzip function
 def yoda_unzip(zips):
@@ -90,26 +131,15 @@ def run_config(args):
         config_exec = subprocess.Popen(['python', path])
         config_exec.wait()
     else:
-        print 'invalid config:'
-        print args
+        error_object.append('invalid config:')
+        error_object.append(args)
+        raise yodaError(error_object)
 
 # execution
-
-env = init()
-# needs to be redone to match new init function
-'''
-if env['config']:
-    if curl_resource(env['args']):
-        print 'All resources downloaded successfully. Extracting archives.'
-        yoda_unzip(env['args'])
-        print 'Running config'
-        run_config(env['args'])
-else:
-    if curl_resource(env['args']):
-        print 'All resources downloaded successfully. Extracting archives.'
-        yoda_unzip(env['args'])
-        
-'''
-
+try:
+    grab(init())
+except yodaError as e:
+    for error_msg  in e.value:
+        print error_msg
 
 
